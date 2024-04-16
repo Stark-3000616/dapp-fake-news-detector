838a2a16ca43fd8a1b902627d2d6b50b72ac6282
Shamik Kumar De <shamikisbatman@gmail.com>
2024-04-16 20:42:23 +0530

    visualsation improved

--- evaluation.py
+++ evaluation.py
@@ -12,8 +12,8 @@ def run_simulation(num_of_voters, trustworthy_pct, malicious_pct, num_of_news_it
 def main():
     num_of_voters = 30
     trustworthy_pct_values = range(0,21,10)
-    num_of_news_items = 100
-    malicious_pct_values = range(0, 81, 10)
+    num_of_news_items = 300
+    malicious_pct_values = range(0, 81, 5)
 
     for trustworthy_pct in trustworthy_pct_values:
 
@@ -26,7 +26,7 @@ def main():
             accuracies.append(accuracy/10)
 
         # Visualize the data
-        plt.plot(malicious_pct_values, accuracies, marker='o', label=f'Trusted voters= {trustworthy_pct}%')
+        plt.plot(malicious_pct_values, accuracies,marker='x', label=f'Trusted voters= {trustworthy_pct}%')
     plt.xlabel('Malicious Voters Percentage')
     plt.ylabel('Accuracy')
     plt.title('Fake News Detection Accuracy vs. Malicious Voters Percentage')
